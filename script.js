a30be031e86dc8ec38bad9061564156619c6c7c8
Stronkness <andrefrisk98@gmail.com>
2023-08-15 18:44:19 +0200

    English language fix

--- script.js
+++ script.js
@@ -67,6 +67,7 @@ const questions = [
             { text: "Inreda" },
             { text: "Testa appar" },
             { text: "Arbeta fysiskt" },
+            { text: "Inget av ovanstående" },
         ],
     },
 ];
@@ -80,7 +81,7 @@ const role_scoring = [
             1: [5, 4, 3, 2, 1],
             2: [5, 4, 3, 2, 1],
             3: [1, 2, 3, 4, 5],
-            4: [0, 0, 0, 0, 0, 0],
+            4: [0, 0, 0, 0, 0, 0, 0],
             Description_SWE:
                 "Som Banquet Host på ARKAD är du del i det lag som organiserar den stora Banketten i AF-Borgen. Du hjälper till och förbereder inför sittningen och på Bankettdagen ser du till så allt blir redo i lokalen för när Banketten drar igång. När allt är ordnat och gäster börjar anlända får du möjlighet att byta till finare kläder och delta i middagen och det följande släppet. Att vara Banquet Host passar perfekt för de som är villiga att lägga lite mer kraft på att göra en bra kväll utmärkt!NNDu behöver vara tillgänglig hela den 15:e november, samt några tillfällen dessförinnan när ni planerar arbetet.",
             Description_ENG:
@@ -91,7 +92,7 @@ const role_scoring = [
             1: [1, 3, 5, 6, 7],
             2: [1, 2, 3, 4, 5],
             3: [1, 2, 3, 4, 5],
-            4: [0, 0, 0, 0, 0, 0],
+            4: [0, 0, 0, 0, 0, 0, 0],
             Description_SWE:
                 "Som Company Host kommer du att bli tilldelad två företag som du kommer att assistera inför och under mässan. Din huvudsakliga uppgift är att hjälpa företaget och vara dess informationskälla. Som Company Host får du en god möjlighet att knyta kontakter med företagen på mässan. Jobbet som Company Host är perfekt för den som vill komma i kontakt med företag och testa på ett professionellt arbetssätt. \n\nNågra veckor innan mässan ska du kontakta företaget och följa upp deras registrering och förbereda dem för den kommande mässan. Du förväntas hjälpa till att förbereda mässlokalerna under helgen innan ARKAD. Dagen innan mässan möter du upp ditt företag och visar dem var de ska stå, för att sedan vara behjälplig under hela mässan. När mässan är slut hjälper alla i ARKAD till med att städa och ställa i ordning allt i husen. \n\nSom Company Host förväntas du vara tillgänglig under båda mässdagarna för att svara på frågor och stötta företagen.",
             Description_ENG:
@@ -102,7 +103,7 @@ const role_scoring = [
             1: [1, 2, 3, 4, 5],
             2: [1, 1, 2, 3, 4],
             3: [5, 4, 3, 2, 2],
-            4: [0, 0, 0, 0, 0, 0],
+            4: [0, 0, 0, 0, 0, 0, 0],
             Description_SWE:
                 "Vill du vara med och organisera större event? Sök Eventvärd! Du kommer hjälpa företagsrepresentanter och dina medvärdar att leverera event veckan innan och under ARKAD. Detta innebär bland annat att du förbereder lokaler inför eventen, att du välkomnar gäster, och att du städar efteråt. Eventvärdar representerar ARKAD mot studenter och företagsrepresentanter. Du kommer få kontakter med företag och ett utlopp för din samarbetsförmåga. \n\nSom Eventvärd behöver du primärt vara tillgänglig att jobba skift några vardagar mellan 6 november–110 november samt 14-15 november.",
             Description_ENG:
@@ -113,7 +114,7 @@ const role_scoring = [
             1: [5, 4, 3, 2, 1],
             2: [5, 4, 3, 2, 1],
             3: [2, 2, 2, 3, 3],
-            4: [0, 5, 0, 0, 0, 0],
+            4: [0, 5, 0, 0, 0, 0, 0],
             Description_SWE:
                 "Du kommer, tillsammans med de andra Film Hosts och Filmkoordinatorerna, dokumentera mässan. Ni är ansvariga för att skapa kortare klipp att publicera på sociala medier under upptakten till samt under mässan. Du kommer också samla video av våra events och mässan för att kunna skapa ARKADs Aftermovie, som tackar företag och deltagande. Att vara en Film Host passar de som är sociala och kreativa. Att ha tidigare erfarenhet av videografi är meriterande. \n\nDu behöver vara tillgänglig några tider i veckorna innan ARKAD, samt under mässan och några tillfällen därefter.",
             Description_ENG:
@@ -124,7 +125,7 @@ const role_scoring = [
             1: [1, 1, 1, 2, 3],
             2: [1, 2, 3, 4, 5],
             3: [5, 4, 3, 2, 1],
-            4: [0, 0, 0, 0, 0, 0],
+            4: [0, 0, 0, 0, 0, 0, 0],
             Description_SWE:
                 "Är du hjälpsam och har lätt att hålla koll på information? Som Info Desk Host står du i en av informationsdiskarna i entrén till våra mässlokaler. Din främsta uppgift är att se till så företagsrepresentanter och studenter får svar på alla frågor som uppstår. Vi söker du som är serviceinriktad och kan möta utmaningar med ett leende.  Före mässan arbetar du med att förbereda infodiskarna så att all information och allt material finns tillgängligt när det väl gäller. Under mässan arbetar du i skift tillsammans med andra värdar i en av infodiskarna.\n\nDu behöver vara tillgänglig helgen innan mässan, samt för att arbeta några pass under mässdagarna.  ",
             Description_ENG:
@@ -135,7 +136,7 @@ const role_scoring = [
             1: [5, 4, 3, 2, 1],
             2: [5, 4, 3, 2, 1],
             3: [1, 2, 3, 4, 5],
-            4: [0, 0, 0, 5, 0, 0],
+            4: [0, 0, 0, 5, 0, 0, 0],
             Description_SWE:
                 "Är du intresserad av att dekorera och tycker det är kul med inredningsdesign? Som Interior Host har du i uppgift att hjälpa till att få mässan att vara estetiskt tilltalande samtidigt som den ger ett professionellt intryck. Större delen av arbetet görs före mässan, med huvuduppgiften att dekorera husen inför mässan. Under själva mässan hjälper du som Interior Host till när det behövs och ser till så att dekoren och husen ser bra ut. Efter mässan hjälper du till att städa undan dekoren.\n\nDu behöver vara tillgänglig att planera veckan innan ARKAD samt på plats på campus under helgen innan mässan då vi dekorerar!",
             Description_ENG:
@@ -146,7 +147,7 @@ const role_scoring = [
             1: [5, 4, 3, 2, 1],
             2: [5, 4, 3, 2, 1],
             3: [1, 2, 3, 4, 5],
-            4: [0, 0, 0, 0, 0, 5],
+            4: [0, 0, 0, 0, 0, 5, 0],
             Description_SWE:
                 "Som Logistics Host kommer du tillsammans med logistikgruppen se till så att allt funkar som det ska under mässdagarna. Ni står för samordning, organisation och transport av företagens gods. Vi söker dig som har en positiv inställning, vill lära dig nya saker, och tycker om att ha koll på grejer. Om du gillar kommunikation och samarbete är den här värdplatsen för dig! Du får knyta kontakter med företag samt att lära känna alla andra studenter i gruppen.\n\nDu behöver vara tillgänglig helgen innan mässan fram tills sista mässdagen.",
             Description_ENG:
@@ -157,7 +158,7 @@ const role_scoring = [
             1: [1, 1, 1, 2, 3],
             2: [1, 2, 3, 4, 5],
             3: [5, 4, 3, 2, 1],
-            4: [0, 0, 0, 0, 0, 0],
+            4: [0, 0, 0, 0, 0, 0, 0],
             Description_SWE:
                 "Som Lounge Host förbereder du och tar hand om loungerna runt ARKAD tillsammans med dina medvärdar. Att vara en bra Lounge-värd handlar om att vara serviceminded och att ha ett leende på läpparna. Du arbetar mestadels under mässan och arbetet handlar mest om att hålla det rent och fräscht och se till att mat och dryck alltid är tillgängliga. Du kommer, tillsammans med dina medvärdar, att ställas inför små och stora hinder som kräver snabba lösningar. Under mässan får du dessutom en god chans att samtala med företagsrepresentanter och andra värdar i en avslappnad miljö. Sök till Lounge Host om du vill bidra till detta fantastiska evenemang genom att skapa den optimala loungen.\n\nDu behöver vara tillgänglig under mässdagarna.",
             Description_ENG:
@@ -168,7 +169,7 @@ const role_scoring = [
             1: [1, 1, 1, 2, 3],
             2: [1, 2, 3, 4, 5],
             3: [5, 4, 3, 2, 1],
-            4: [0, 0, 0, 0, 0, 0],
+            4: [0, 0, 0, 0, 0, 0, 0],
             Description_SWE:
                 "Som Lunch Host jobbar du huvudsakligen med att ta hand om gästerna, duka, servera mat och se till att lunchområdet är fint och välkomnande under hela lunchen. Luch Hosts är också ansvariga för entrén till lunchen och se till att gästerna kommer under sin tilldelade tid. Du jobbar främst under mässans två dagar men trots det kommer tid finnas till att besöka mässan på eftermiddagen. Då alla mässans deltagare kommer till lunchen får du som Lunch Host chans att träffa många nya människor, både studenter och företagsrepresentanter.\n\nDu behöver vara tillgänglig under mässdagarna.",
             Description_ENG:
@@ -179,7 +180,7 @@ const role_scoring = [
             1: [5, 4, 3, 2, 1],
             2: [5, 4, 3, 2, 1],
             3: [1, 2, 3, 4, 5],
-            4: [0, 0, 5, 0, 0, 0],
+            4: [0, 0, 5, 0, 0, 0, 0],
             Description_SWE:
                 "Som Network and Connection Host kommer du att vara en del i arbetet med allt som rör el och nätverk under mässan. Du kommer inför mässan vara delaktig i planeringen kring hur all el och nätverk skall förläggas, samt även att hjälpa till att montera allting. Under mässan så kommer du, tillsammans med andra värdar, arbeta under ett pass per dag med uppgift att lösa eventuella problem som kan uppstå. När mässan är slut så kommer du hjälpa till att återställa lokalerna till ordinarie skick genom att packa ihop allt kablage som tidigare blivit utplacerat. Ditt bidrag till ARKAD kommer vara oersättligt och uppskattat!\n\nDu behöver vara tillgänglig 10–11 november, när företagen anländer kvällen innan mässan, samt för att jobba några pass under mässdagarna.",
             Description_ENG:
@@ -190,7 +191,7 @@ const role_scoring = [
             1: [5, 4, 3, 2, 1],
             2: [5, 4, 3, 2, 1],
             3: [2, 2, 2, 3, 3],
-            4: [5, 0, 0, 0, 0, 0],
+            4: [5, 0, 0, 0, 0, 0, 0],
             Description_SWE:
                 "Som Photo Host är din uppgift att dokumentera allt kring ARKAD; events, lunchföreläsningar, Banketten, mässområdet. Tillsammans med PR-gruppen väljs sedan bilder ut att användas i framställningen av årets och framtida ARKAD. Som Photo Host passar du perfekt som är kreativ, effektiv, och har ett öga för bildkomposition. Det är positivt om du har tidigare erfarenhet av fotografering.\n\nDina arbetstider kommer främst vara vecka 45, eventveckan, och under mässan.",
             Description_ENG:
@@ -201,7 +202,7 @@ const role_scoring = [
             1: [1, 2, 3, 4, 5],
             2: [1, 2, 3, 4, 5],
             3: [1, 2, 2, 2, 3],
-            4: [0, 0, 0, 0, 0, 0],
+            4: [0, 0, 0, 0, 0, 0, 0],
             Description_SWE:
                 "Student Sessions är ARKADs kontaktsamtal mellan företag och studenter. Vi behöver dig som är en hjälpsam och social person som Student Sessions Host! Du kommer ingå i det värdlag som inför mässan ordnar rummen kontaktsamtalen sker i; under ARKAD jobbar du direkt med företagsrepresentanter och studenter så att alla kan få ut så mycket som möjligt från kontaktsamtalen genom att hjälpa deltagande och komma med information och assistans när det behövs. \n\nSom Student Sessions Host behöver du vara tillgänglig från den att ordna rummen under helgen 12–13 november, samt under mässan 15–16 november.",
             Description_ENG:
@@ -212,7 +213,7 @@ const role_scoring = [
             1: [1, 1, 1, 2, 3],
             2: [1, 1, 2, 3, 4],
             3: [6, 5, 4, 2, 1],
-            4: [0, 0, 0, 0, 0, 0],
+            4: [0, 0, 0, 0, 0, 0, 0],
             Description_SWE:
                 "Som Wardrobe Host, tillsammans med andra Wardrobe Hosts, håller du koll på en av våra garderober där företagsrepresentanter kan lämna och hämta sina ytterkläder. Din huvudsakliga uppgift är hantering av garderobsbiljetter. Ni arbetar främst under öppningen och stängningen av mässan då det är som mest folk i rörelse. Ni får välkomna alla företag till ARKAD och är de första personerna de träffar under mässan. Eftersom ni mestadels jobbar under öppning och stängning kommer du ha tid att utforska ARKAD under dagen. För denna posten behöver du vara serviceminded för att se till så företagen får ett bra första intryck på mässan.\n\nDu behöver vara tillgänglig för att jobba några skift under mässdagarna, 15e-16e november.",
             Description_ENG:
@@ -223,7 +224,7 @@ const role_scoring = [
             1: [4, 4, 3, 2, 1],
             2: [4, 4, 3, 2, 1],
             3: [1, 2, 3, 4, 4],
-            4: [0, 0, 0, 0, 5, 0],
+            4: [0, 0, 0, 0, 5, 0, 0],
             Description_SWE: "Beep boop",
             Description_ENG: "Beep boop (in English)",
         },
@@ -262,7 +263,6 @@ function prepare() {
 }
 
 function language_selector_english() {
-    console.log("english");
     document.getElementById("swedish").remove();
     document.getElementById("english").remove();
     document.getElementById("choose_language").remove();
@@ -299,6 +299,7 @@ function load_question() {
         { text: "Decoration" },
         { text: "Test Apps" },
         { text: "Work physically" },
+        { text: "None of the above" },
     ];
 
     for (let i = 0; i < questions[current_question].answer.length; i++) {
@@ -345,8 +346,6 @@ function load_score() {
         }
     }
 
-    console.log(top_three, three_role_keys);
-
     let three_roles_with_description = [];
     for (let i = 0; i < three_role_keys.length; i++) {
         const key = three_role_keys[i];
@@ -373,8 +372,13 @@ function load_score() {
     const role_3 = document.getElementById("role_3");
     const description_3 = document.getElementById("description_3");
 
-    welcome.textContent =
-        "Värdkompassen tror att du kommer passa bäst med rollen som:\n\n";
+    if (language == "SWE") {
+        welcome.textContent =
+            "Värdkompassen tror att du kommer passa bäst med rollen som:\n\n";
+    } else {
+        welcome.textContent =
+            "The host compass believes that you will fit best with the role of";
+    }
     role_1.textContent = `${three_roles_with_description[0][0]}\n`;
     description_1.textContent = `${three_roles_with_description[0][1]}\n\n`;
     role_2.textContent = `${three_roles_with_description[1][0]}\n`;
